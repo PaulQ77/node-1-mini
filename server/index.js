60077143e28f9b4d3f0bb1e0aea23ccc3fedad3d
Paul Quiroz <pquiroz1977@gmail.com>
2018-05-09 16:33:13 -0700

    step 9

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bc = require('./controllers/books_controller');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.get('/api/books', bc.read);
+
+app.post('/api/books', bc.create);
+
+app.put('/api/books/:id', bc.update);
+
+app.delete('api/books/:id', bc.delete);
+
+
+const port = 3000
+app.listen(port, () => console.log(`We're rollin' on port ${port}`))
